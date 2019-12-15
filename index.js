fbf3cab304f255bab738c62d06644525446764c0
silvodesigns <kevsilvo@gmail.com>
2019-12-15 00:04:54 -0500

    added put request

--- index.js
+++ index.js
@@ -84,6 +84,32 @@ server.get('/api/users/:id', (req, res) => {
 
 
 
+//updates user from database
+  server.put('/api/users/:id', (req, res) => {
+    const { name, bio } = req.body;
+  
+    if (!name || !bio) {
+      res.status(400).json({ errorMessage: 'Please provide name and bio for the user.' });
+    } else {
+      db.update(req.params.id, req.body)
+        .then(user => {
+          if (user) {
+            res.status(200).json(user);
+          } else {
+            res.status(404).json({ message: 'The user with the specified ID does not exist.'});
+          }
+        })
+        .catch(() => {
+          res.status(500).json({
+            errorMessage: 'The user information could not be modified.',
+          });
+        });
+    }
+  });
+
+
+
+
 server.listen(8000, () => {
     console.log("API Running on port 8000")
 })
